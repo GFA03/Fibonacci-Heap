d034a17e7250ab1461351800eae7a1e42e0cc607
Gavrila Alexandru <florinalexandru302003@gmail.com>
2024-01-18 08:30:27 +0200

    included a new library

--- FibonacciHeap.h
+++ FibonacciHeap.h
@@ -9,42 +9,46 @@
 #include <iostream>
 #include <unordered_map>
 #include <algorithm>
+#include <cmath>
 
-struct Node{
+struct Node
+{
     int value;
     int degree;
     bool marked;
-    Node* left;
-    Node* right;
-    Node* parent;
-    Node* child;
+    Node *left;
+    Node *right;
+    Node *parent;
+    Node *child;
 
     explicit Node(int value);
-    void appendChild(Node* newChild);
-    static void linkNodes(Node* baseNode, Node* linkedNode);
-    static void merge(Node* node1, Node* node2);
+    void appendChild(Node *newChild);
+    static void linkNodes(Node *baseNode, Node *linkedNode);
+    static void merge(Node *node1, Node *node2);
 };
 
-class FibonacciHeap{
-    Node* minNode;
+class FibonacciHeap
+{
+    Node *minNode;
     int numberOfNodes;
-    std::vector<Node*> nodeList;
-public:
-    FibonacciHeap():minNode(nullptr), numberOfNodes(0){}
+    std::vector<Node *> nodeList;
 
-    friend std::ostream& operator<<(std::ostream& out, const FibonacciHeap& obj);
+public:
+    FibonacciHeap() : minNode(nullptr), numberOfNodes(0) {}
+
+    friend std::ostream &operator<<(std::ostream &out, const FibonacciHeap &obj);
 
     int findMin();
-//    void merge(Node* root1, Node* root2);
+    //    void merge(Node* root1, Node* root2);
     void insert(int value);
-    static Node* mergeTrees(Node* root1, Node* root2, std::vector<Node*>& degrees);
+    static Node *mergeTrees(Node *root1, Node *root2, std::vector<Node *> &degrees);
     int extractMin();
-    void cutChild(Node* parent, Node* child);
-    void decreaseKey(Node* node, int newValue);
+    void cutChild(Node *parent, Node *child);
+    void decreaseKey(Node *node, int newValue);
 
-    Node* getNode(int index) const;
+    Node *getNode(int index) const;
 
     void printNodeList();
 };
 
-#endif //FIBONACCIHEAPS_FIBONACCIHEAP_H
+#endif // FIBONACCIHEAPS_FIBONACCIHEAP_H
